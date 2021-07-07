a085574d64ddca477155c2111dd4e883193a0c8f
Daiane <5314424+daianeczm@users.noreply.github.com>
2021-07-07 19:51:36 -0300

    Add files via upload

---
+++ testes2.py
@@ -0,0 +1,8 @@
+contador = 0 
+somador = 0 
+while contador < 5: 
+    contador = contador + 1 
+    valor = float(input('Digite o '+str(contador)+'º valor: ')) 
+    somador = somador + valor 
+print('Soma = ', somador) 
+
